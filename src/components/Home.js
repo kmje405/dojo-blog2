c26516f528efdd8663f499d589040a1cc3657529
K.M. Jones-Eastland <kmje405@gmail.com>
2023-07-18 18:57:54 -0500

    add json server and sample blog data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,31 @@
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from "react";
+import BlogList from "./BlogList";
+
 const Home = () => {
-  return (
-    <div className="home">
-      <h2>Homepage</h2>
-    </div>
-  );
+    const [blogs, setBlogs] = useState(null);
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    };
+
+    useEffect(() => {
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                setBlogs(data);
+            })
+    }, []);
+
+
+    return (
+        <div className="home">
+            {blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />}
+        </div>
+    );
 };
 
 export default Home;
