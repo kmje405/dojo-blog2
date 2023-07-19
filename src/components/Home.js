b5d42b2a01acf78b836747fd08191967d30daf5a
K.M. Jones-Eastland <kmje405@gmail.com>
2023-07-19 12:18:09 -0500

    updates

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
@@ -7,18 +6,24 @@ const Home = () => {
     const [isPending, setIsPending] = useState(true);
 
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-            .then(res => {
-                return res.json();
-            })
-            .then(data => {
-                setBlogs(data);
-            })
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
+                .then(res => {
+                    return res.json();
+                })
+                .then(data => {
+                    setBlogs(data);
+                    setIsPending(false);
+                });
+                .catch(err => {
+                    console.log(err.message);
+                })
+        }, 1000)
     }, []);
 
     return (
         <div className="home">
-            { isPending && <div>Loading...</div> }
+            {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs" />}
         </div>
     );
